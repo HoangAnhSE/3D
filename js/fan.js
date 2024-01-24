34cb1b22f15f5f68e0c47cf60c8d443d45a53f7f
HoangAnh <112847236+HoangAnhSE@users.noreply.github.com>
2024-01-24 12:05:18 +0700

    Delete js directory

--- js/fan.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const buttonFan = document.querySelector('#buttonFan');
-    buttonFan.addEventListener('click', function() {
-      const ceilingFan = document.querySelector('#ceilingFan');
-      ceilingFan.emit('rotateFan');
-    });
-  });
-  
